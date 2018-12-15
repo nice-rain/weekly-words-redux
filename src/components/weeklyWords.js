4de1262836f11cea53694906cb942aa8c31b2bd7
NiceRain <walk12288@gmail.com>
2018-12-15 11:39:12 -0600

    Setup basic, static, responsive container for application.

---
+++ src/components/weeklyWords.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './weeklyWords.css';
+
+class WeeklyWords extends Component {
+  render() {
+    return (
+      <div className="Container">
+        <header className="App-header">
+          <h1>Weekly Words</h1>
+        </header>
+        <main>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+
+            <p>Deck List Here</p>
+            <p>Deck List Here</p>
+
+        </main>
+        <footer>
+            Need Help?
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default WeeklyWords;
