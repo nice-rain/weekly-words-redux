a5879061c8fc3615d2cd8792c138fd60df7c0453
NiceRain <walk12288@gmail.com>
2018-12-31 19:08:45 -0600

    Setup redux-form for login page.

--- src/components/login.js
+++ src/components/login.js
@@ -1,14 +1,26 @@
 //Displays the review word and button to flip card
 import React from 'react';
 import './login.css';
-import {connect} from 'react-redux';
+import {reduxForm, Field} from 'redux-form';
 //import {doLogin} from '../actions';
 
 export function Login(props)
 {
+    function onSubmit(values) {
+        console.log(values);
+    }
+
     return (
-        <div>Login</div>
+        <form onSubmit={props.handleSubmit(values => onSubmit(values))}>
+            <label htmlFor="name">Username</label>
+            <Field name="name" id="name" type="text" component="input" />
+            <label htmlFor="email">Password</label>
+            <Field name="email" id="email" type="email" component="input" />
+            <button type="submit">Login</button>
+        </form>
     );
 }
 
-export default connect()(Login);
+export default reduxForm({
+    form: 'login'
+})(Login);
