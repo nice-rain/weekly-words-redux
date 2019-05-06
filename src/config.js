5457e1478a211cca26917bed93ac07e31115ebd0
Walker Twyman <walker.twyman@bottlerocketstudios.com>
2019-05-06 15:47:10 -0500

    Updated link to new API

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8080,
     // other stuff
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-      "http://localhost:8083/ww/api"
+      "https://weekly-words-api.herokuapp.com/ww/api"
   };
